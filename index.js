e15c45f6d9da460542e3102374e04d8a32d9b96c
kitety <aizaizuori@foxmail.com>
2019-04-01 00:14:53 +0800

    let var 区别

--- index.js
+++ index.js
@@ -1,5 +1,19 @@
 // var fn = () => 'Hello'
 var a = 13;
 var b = true;
-var c = 'Hello';
+var c = 'Hello' + 6;
 a = 45;
+for (var i = 0; i < 10; i++) {
+    // console.log(i)
+}
+// Error
+// console.log(i)
+if (true) {
+    var j_1 = 0;
+}
+var j = 12;
+// console.log(j)
+{
+    var my_name = "Kitety";
+    var my_name1 = "Kitety1";
+}
--- index.ts
+++ index.ts
@@ -1,5 +1,22 @@
 // var fn = () => 'Hello'
 var a: number = 13
 var b: boolean = true
-var c: string = 'Hello'
+var c: string = 'Hello' + 6
 a = 45
+
+for (let i = 0; i < 10; i++) {
+  // console.log(i)
+}
+// Error
+// console.log(i)
+
+if (true) {
+  let j = 0
+}
+var j: number = 12;
+// console.log(j)
+
+{
+  let my_name: string = "Kitety"
+  let my_name1: string = "Kitety1"
+}
