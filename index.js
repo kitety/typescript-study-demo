050641e3b2dc83a474b0e902dc97c2aea61340ba
kitety <aizaizuori@foxmail.com>
2019-04-29 15:03:38 +0800

    只读属性

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ var __extends = (this && this.__extends) || (function () {
 // var fn = () => 'Hello'
 var a = 13;
 var b = true;
-var c = 'Hello' + 6;
+var c = "Hello" + 6;
 a = 45;
 for (var i = 0; i < 10; i++) {
     // console.log(i)
@@ -32,7 +32,7 @@ var j = 12;
 }
 // Array
 {
-    var myArr1 = [1, 2, ''];
+    var myArr1 = [1, 2, ""];
     var myArr2 = [1, 2, 3];
     var myArr3 = [1, 2, 3, false, 4];
     myArr3.push(12);
@@ -42,7 +42,7 @@ var j = 12;
 }
 // tuple
 {
-    var tuple1 = ['string', 1, false];
+    var tuple1 = ["string", 1, false];
     // 在后面可以追加，但是要是前面已经有了的类型，强而且不能插入，
     // 前面的几个类型必须满足，顺序要固定
     tuple1.push(2);
@@ -67,7 +67,7 @@ function sum(a, b) {
         rest[_i - 2] = arguments[_i];
     }
     // a+b为初始值
-    var restAll = rest.reduce(function (total, num) { return (total + num); }, a + b);
+    var restAll = rest.reduce(function (total, num) { return total + num; }, a + b);
     return restAll;
 }
 console.log(sum(45, 1, 2, 3, 4, 5, 6));
@@ -75,18 +75,18 @@ console.log(sum(45, 1, 2, 3, 4, 5, 6));
 function isNumber(value) {
     // ts对应的类型判断
     // 可以进行进一步处理
-    return typeof value === 'number';
+    return typeof value === "number";
 }
-console.log(1111, isNumber('11'));
+console.log(1111, isNumber("11"));
 function isString(value) {
-    return typeof value === 'string';
+    return typeof value === "string";
 }
 // any
 {
     // 慎用
     var a_1;
     a_1 = 10;
-    a_1 = 'String';
+    a_1 = "String";
     a_1 = [1, 23, 456];
     var log = function (value) {
         if (isNumber(value)) {
@@ -99,11 +99,11 @@ function isString(value) {
             throw new Error("Expected string or number,but get " + value + ".");
         }
     };
-    console.log(log('qqq'));
+    console.log(log("qqq"));
     var c_1;
     // 报错
     // c=12;
-    c_1 = [1, [], 1, 'Hello'];
+    c_1 = [1, [], 1, "Hello"];
 }
 // 联合类型
 {
@@ -132,7 +132,7 @@ function isString(value) {
     }());
     var aPerson = new Person();
     // 设置内容
-    aPerson.firstName = 'Kitety';
+    aPerson.firstName = "Kitety";
     aPerson.age = 23;
     console.log(aPerson.firstName);
     var Movie = /** @class */ (function () {
@@ -155,8 +155,8 @@ function isString(value) {
         return Movie;
     }());
     // 声称对象
-    var movie = new Movie('电影', 15, 87);
-    movie.name = 'Avengers';
+    var movie = new Movie("电影", 15, 87);
+    movie.name = "Avengers";
     console.log(movie.display_play_count());
     movie.increse_play_count();
     console.log(movie.display_play_count());
@@ -167,7 +167,7 @@ function isString(value) {
         function Person() {
         }
         Person.prototype.greet = function () {
-            console.log('hi!');
+            console.log("hi!");
         };
         Person.prototype.otherGreet = function () {
             this.greet();
@@ -180,7 +180,7 @@ function isString(value) {
             return _super !== null && _super.apply(this, arguments) || this;
         }
         Programmer.prototype.greet = function () {
-            console.log('hello world!');
+            console.log("hello world!");
         };
         // super 代表父类
         Programmer.prototype.greetLikeNormalPerson = function () {
@@ -203,13 +203,13 @@ function isString(value) {
         function Person() {
         }
         Person.prototype.greet = function () {
-            console.log('hi!');
+            console.log("hi!");
         };
         Person.prototype.getFirstName = function () {
             return this.firstName;
         };
         Person.prototype.sayHi = function () {
-            console.log('private say hi!');
+            console.log("private say hi!");
         };
         Person.prototype.sayHiOut = function () {
             this.sayHi();
@@ -242,7 +242,7 @@ function isString(value) {
     // 2.private
     var aPerson = new Person();
     // 错误,只能在class Person内部使用
-    console.log(aPerson.setFirstName('Kitety'));
+    console.log(aPerson.setFirstName("Kitety"));
     console.log(aPerson.getFirstName());
     aPerson.sayHiOut();
 }
@@ -253,13 +253,13 @@ function isString(value) {
         function Person() {
         }
         Person.prototype.greet = function () {
-            console.log('hi!');
+            console.log("hi!");
         };
         Person.prototype.getFirstName = function () {
             return this.firstName;
         };
         Person.prototype.sayHi = function () {
-            console.log('private say hi!');
+            console.log("private say hi!");
         };
         Person.prototype.sayHiOut = function () {
             this.sayHi();
@@ -278,7 +278,7 @@ function isString(value) {
             return _super !== null && _super.apply(this, arguments) || this;
         }
         Programmer.prototype.greet = function () {
-            console.log('hello world!');
+            console.log("hello world!");
         };
         // super 代表父类
         Programmer.prototype.greetLikeNormalPerson = function () {
@@ -301,7 +301,7 @@ function isString(value) {
     var aProgrammer = new Programmer();
     // 报错
     // console.log(aProgrammer.firstName);
-    aProgrammer.setName('kitety');
+    aProgrammer.setName("kitety");
     console.log(aProgrammer.getFullName());
     console.log(aProgrammer.showName());
     // 测试是不行的
@@ -327,7 +327,7 @@ function isString(value) {
             this.lastName = lastName;
         }
         Person.prototype.greet = function () {
-            console.log('hi!');
+            console.log("hi!");
         };
         return Person;
     }());
@@ -339,17 +339,78 @@ function isString(value) {
             var _this = 
             // 调用父类构造函数的方法
             _super.call(this, firstName, lastName) || this;
-            console.log('programer constructor');
+            console.log("programer constructor");
             return _this;
         }
         return Programmer;
     }(Person));
     // protected constructor就会出现错误,并且子类继承生成实例也会报错
     // let per = new Person('hello', 'kitety')
-    var pro = new Programmer('hello', 'kitety');
+    var pro = new Programmer("hello", "kitety");
     /**
      * public 默认,子类可用
      * protected 子类可重写contructor配合super使用
      * private 子类不可用
      */
+}
+// static
+// 不用实例化
+/**
+ * 通过类.属性，或者类.方法来调用，
+ * protected/private 当前类不能调用，只能通过public静态方法方法调用
+ * 父类protected/private 子类继承所有,子类不能直接调用protected/private属性，只能通过继承的public方法调用
+ * 子类定义的方法，只要有是父类定义为public protected的才能使用，private的不能
+ */
+{
+    var Person_1 = /** @class */ (function () {
+        function Person() {
+        }
+        // public protected private一样的
+        Person.getStaticAge = function () {
+            return Person.age;
+        };
+        Person.prototype.getAge = function () {
+            return Person.age;
+        };
+        // 静态属性
+        Person.age = 10;
+        Person.name1 = "hello";
+        return Person;
+    }());
+    var p = new Person_1();
+    // console.log("静态属性", Person.age);
+    console.log("静态方法", Person_1.getStaticAge());
+    console.log("静态属性调用", p.getAge());
+    var Programmer_1 = /** @class */ (function (_super) {
+        __extends(Programmer, _super);
+        function Programmer() {
+            var _this = 
+            // 调用父类构造函数的方法
+            _super.call(this) || this;
+            console.log("programer constructor");
+            return _this;
+        }
+        // static getProgrammerAge(): number {
+        //   return Programmer.age;
+        // }
+        Programmer.getProgrammerName = function () {
+            return Programmer.name1;
+        };
+        return Programmer;
+    }(Person_1));
+    console.log("getProgrammerAge", Programmer_1.getStaticAge());
+    console.log("getProgrammerName", Programmer_1.getProgrammerName());
+}
+// readonly 只读属性
+{
+    var Person = /** @class */ (function () {
+        function Person() {
+            this.name = "readonly name string";
+        }
+        return Person;
+    }());
+    var aPerson = new Person();
+    // 会报错，只读属性
+    aPerson.name = "hello";
+    console.log(aPerson.name);
 }
--- index.ts
+++ index.ts
@@ -1,8 +1,8 @@
 // var fn = () => 'Hello'
-var a: number = 13
-var b: boolean = true
-var c: string = 'Hello' + 6
-a = 45
+var a: number = 13;
+var b: boolean = true;
+var c: string = "Hello" + 6;
+a = 45;
 
 for (let i = 0; i < 10; i++) {
   // console.log(i)
@@ -11,99 +11,99 @@ for (let i = 0; i < 10; i++) {
 // console.log(i)
 
 if (true) {
-  let j = 0
+  let j = 0;
 }
 var j: number = 12;
 // console.log(j)
 
 {
-  let my_name: string = "Kitety"
-  let my_name1: string = "Kitety1"
+  let my_name: string = "Kitety";
+  let my_name1: string = "Kitety1";
 }
 
 // Array
 {
-  let myArr1: (number | string)[] = [1, 2, '']
-  let myArr2: (number)[] = [1, 2, 3]
+  let myArr1: (number | string)[] = [1, 2, ""];
+  let myArr2: (number)[] = [1, 2, 3];
   let myArr3: Array<number | boolean> = [1, 2, 3, false, 4];
-  myArr3.push(12)
+  myArr3.push(12);
   // 赋值就不用定义类型,因为声明的时候定义了
-  let popElement = myArr3.pop()
-  myArr3 = []
+  let popElement = myArr3.pop();
+  myArr3 = [];
 }
 // tuple
 {
-  let tuple1: [string, number, boolean] = ['string', 1, false]
+  let tuple1: [string, number, boolean] = ["string", 1, false];
   // 在后面可以追加，但是要是前面已经有了的类型，强而且不能插入，
   // 前面的几个类型必须满足，顺序要固定
-  tuple1.push(2)
+  tuple1.push(2);
   // tuple1.push([])
   // tuple1=[]
   // console.log(tuple1)
 }
 // function
 // 加问号可传可不传
-function add(a: number, b: number = 20, ): number {
+function add(a: number, b: number = 20): number {
   if (!b) {
-    b = 0
+    b = 0;
   }
   return a + b;
 }
-console.log(add(45))
+console.log(add(45));
 
 // ...rest 扩充其他的元素
 function sum(a: number, b: number, ...rest: number[]): number {
   // a+b为初始值
-  let restAll: number = rest.reduce((total, num) => (total + num), a + b)
-  return restAll
+  let restAll: number = rest.reduce((total, num) => total + num, a + b);
+  return restAll;
 }
-console.log(sum(45, 1, 2, 3, 4, 5, 6))
+console.log(sum(45, 1, 2, 3, 4, 5, 6));
 
 // 联合类型
 function isNumber(value: any): value is number {
   // ts对应的类型判断
   // 可以进行进一步处理
-  return typeof value === 'number'
+  return typeof value === "number";
 }
-console.log(1111, isNumber('11'))
+console.log(1111, isNumber("11"));
 function isString(value: any): value is string {
-  return typeof value === 'string'
+  return typeof value === "string";
 }
 // any
 {
   // 慎用
   let a: any;
   a = 10;
-  a = 'String';
-  a = [1, 23, 456]
+  a = "String";
+  a = [1, 23, 456];
   const log = (value: any) => {
     if (isNumber(value)) {
-      return `Your number is ${value}`
+      return `Your number is ${value}`;
     } else if (isString(value)) {
-      return `Your string is ${value}`
+      return `Your string is ${value}`;
     } else {
-      throw new Error(`Expected string or number,but get ${value}.`)
+      throw new Error(`Expected string or number,but get ${value}.`);
     }
-  }
-  console.log(log('qqq'))
+  };
+  console.log(log("qqq"));
   let c: any[];
   // 报错
   // c=12;
-  c = [1, [], 1, 'Hello']
+  c = [1, [], 1, "Hello"];
 }
 // 联合类型
 {
   const log = (value: string | number | null | undefined) => {
     if (isNumber(value)) {
-      return `Your number is ${value}`
+      return `Your number is ${value}`;
     } else if (isString(value)) {
-      return `Your string is ${value}`
+      return `Your string is ${value}`;
     } else {
       // throw new Error(`Expected string or number,but get ${value}.`)
     }
-  }
+  };
   // 这样的话TS都会直接报错
-  console.log(22, log(null))
+  console.log(22, log(null));
   // 还有--strictNullChecks 在编译的时候null只能归于null,undefined只能归于undefined
 }
 // class
@@ -115,38 +115,38 @@ function isString(value: any): value is string {
     lastName: string;
     age: number;
   }
-  let aPerson = new Person()
+  let aPerson = new Person();
   // 设置内容
-  aPerson.firstName = 'Kitety'
-  aPerson.age = 23
+  aPerson.firstName = "Kitety";
+  aPerson.age = 23;
   console.log(aPerson.firstName);
 
   class Movie {
     name: string;
-    play_count: number
-    time: number
+    play_count: number;
+    time: number;
     // this指向生成的对象自身
     constructor(name: string, play_count: number, time: number) {
-      this.name = name
-      this.play_count = play_count
+      this.name = name;
+      this.play_count = play_count;
     }
     // 可能对数据进行操作
     display_play_count(padding?: string) {
       if (padding) {
-        return `${this.play_count} 次 ${padding}`
+        return `${this.play_count} 次 ${padding}`;
       }
-      return `${this.play_count} 次`
+      return `${this.play_count} 次`;
     }
     // 对数据进行操作
     increse_play_count() {
-      this.play_count += 1
+      this.play_count += 1;
     }
   }
   // 声称对象
-  let movie = new Movie('电影', 15, 87)
-  movie.name = 'Avengers'
+  let movie = new Movie("电影", 15, 87);
+  movie.name = "Avengers";
   console.log(movie.display_play_count());
-  movie.increse_play_count()
+  movie.increse_play_count();
   console.log(movie.display_play_count());
 }
 // 继承和多态
@@ -157,28 +157,28 @@ function isString(value: any): value is string {
     lastName: string;
     age: number;
     greet() {
-      console.log('hi!');
+      console.log("hi!");
     }
     otherGreet() {
-      this.greet()
+      this.greet();
     }
   }
   class Programmer extends Person {
     greet() {
-      console.log('hello world!');
+      console.log("hello world!");
     }
     // super 代表父类
     greetLikeNormalPerson() {
-      super.greet()
+      super.greet();
     }
   }
   // class 作为类型
-  let aPerson: Person = new Person()
-  let aProgrammer: Programmer = new Programmer()
+  let aPerson: Person = new Person();
+  let aProgrammer: Programmer = new Programmer();
   // 设置内容
-  aPerson.greet()
-  aProgrammer.greet()
-  aProgrammer.greetLikeNormalPerson()
+  aPerson.greet();
+  aProgrammer.greet();
+  aProgrammer.greetLikeNormalPerson();
 }
 // 成员可见性
 {
@@ -189,22 +189,22 @@ function isString(value: any): value is string {
     private lastName: string;
     private age: number;
     public greet() {
-      console.log('hi!');
+      console.log("hi!");
     }
     public getFirstName() {
-      return this.firstName
+      return this.firstName;
     }
     private sayHi() {
-      console.log('private say hi!');
+      console.log("private say hi!");
     }
     public sayHiOut() {
-      this.sayHi()
+      this.sayHi();
     }
     public setFirstName(name: string) {
-      this.firstName = name
+      this.firstName = name;
     }
     public otherGreet() {
-      this.greet()
+      this.greet();
     }
   }
   // 1.public
@@ -228,11 +228,11 @@ function isString(value: any): value is string {
 
   // 2.private
 
-  let aPerson = new Person()
+  let aPerson = new Person();
   // 错误,只能在class Person内部使用
-  console.log(aPerson.setFirstName('Kitety'));
+  console.log(aPerson.setFirstName("Kitety"));
   console.log(aPerson.getFirstName());
-  aPerson.sayHiOut()
+  aPerson.sayHiOut();
 }
 // protected
 {
@@ -245,41 +245,41 @@ function isString(value: any): value is string {
     protected lastName: string;
     private age: number;
     public greet() {
-      console.log('hi!');
+      console.log("hi!");
     }
     public getFirstName() {
-      return this.firstName
+      return this.firstName;
     }
     private sayHi() {
-      console.log('private say hi!');
+      console.log("private say hi!");
     }
     public sayHiOut() {
-      this.sayHi()
+      this.sayHi();
     }
     public setFirstName(name: string) {
-      this.firstName = name
+      this.firstName = name;
     }
     public otherGreet() {
-      this.greet()
+      this.greet();
     }
   }
   class Programmer extends Person {
     public greet() {
-      console.log('hello world!');
+      console.log("hello world!");
     }
     // super 代表父类
     public greetLikeNormalPerson() {
-      super.greet()
+      super.greet();
     }
     public showName() {
-      return this.firstName
+      return this.firstName;
     }
     public setName(name: string) {
-      this.firstName = name
+      this.firstName = name;
       // this.lastName = 'lastName' + name
     }
     public getFullName(): string {
-      return this.firstName + this.lastName
+      return this.firstName + this.lastName;
     }
     // 这种方式还是不行的 会直接报错
     // public getSuperFirstName():string{
@@ -287,11 +287,11 @@ function isString(value: any): value is string {
     // }
   }
   // class 作为类型
-  let aPerson: Person = new Person()
-  let aProgrammer: Programmer = new Programmer()
+  let aPerson: Person = new Person();
+  let aProgrammer: Programmer = new Programmer();
   // 报错
   // console.log(aProgrammer.firstName);
-  aProgrammer.setName('kitety')
+  aProgrammer.setName("kitety");
   console.log(aProgrammer.getFullName());
   console.log(aProgrammer.showName());
   // 测试是不行的
@@ -314,13 +314,13 @@ function isString(value: any): value is string {
     protected firstName: string;
     protected lastName: string;
     public greet() {
-      console.log('hi!');
+      console.log("hi!");
     }
     // protected constructor就会出现错误,并且子类继承生成实例也会报错
     // private的话子类就直接不能使用super
     protected constructor(firstName: string, lastName: string) {
-      this.firstName = firstName
-      this.lastName = lastName
+      this.firstName = firstName;
+      this.lastName = lastName;
     }
   }
   // protected constructor就会出现错误,并且子类继承生成实例也会报错
@@ -328,17 +328,67 @@ function isString(value: any): value is string {
   class Programmer extends Person {
     public constructor(firstName: string, lastName: string) {
       // 调用父类构造函数的方法
-      super(firstName, lastName)
-      console.log('programer constructor')
+      super(firstName, lastName);
+      console.log("programer constructor");
     }
   }
   // protected constructor就会出现错误,并且子类继承生成实例也会报错
   // let per = new Person('hello', 'kitety')
-  let pro = new Programmer('hello', 'kitety')
+  let pro = new Programmer("hello", "kitety");
   /**
    * public 默认,子类可用
    * protected 子类可重写contructor配合super使用
    * private 子类不可用
    */
 }
-
+// static
+// 不用实例化
+/**
+ * 通过类.属性，或者类.方法来调用，
+ * protected/private 当前类不能调用，只能通过public静态方法方法调用
+ * 父类protected/private 子类继承所有,子类不能直接调用protected/private属性，只能通过继承的public方法调用
+ * 子类定义的方法，只要有是父类定义为public protected的才能使用，private的不能
+ */
+{
+  class Person {
+    // 静态属性
+    private static age: number = 10;
+    protected static name1: string = "hello";
+    // public protected private一样的
+    public static getStaticAge(): number {
+      return Person.age;
+    }
+    public getAge(): number {
+      return Person.age;
+    }
+  }
+  let p = new Person();
+  // console.log("静态属性", Person.age);
+  console.log("静态方法", Person.getStaticAge());
+  console.log("静态属性调用", p.getAge());
+  class Programmer extends Person {
+    public constructor() {
+      // 调用父类构造函数的方法
+      super();
+      console.log("programer constructor");
+    }
+    // static getProgrammerAge(): number {
+    //   return Programmer.age;
+    // }
+    static getProgrammerName(): string {
+      return Programmer.name1;
+    }
+  }
+  console.log("getProgrammerAge", Programmer.getStaticAge());
+  console.log("getProgrammerName", Programmer.getProgrammerName());
+}
+// readonly 只读属性
+{
+  class Person {
+    readonly name: string = "readonly name string";
+  }
+  let aPerson = new Person();
+  // 会报错，只读属性
+  aPerson.name = "hello";
+  console.log(aPerson.name);
+}
